0ff749a714e0a64e30440c2c7b6a45ba5fef88c2
Emily Baird <emilym.baird@gmail.com>
2022-06-27 11:15:41 +0100

    ex 16

--- unittesting/basic-testing-frontend/src/math.test.js
+++ unittesting/basic-testing-frontend/src/math.test.js
@@ -1,7 +1,8 @@
-import { it } from "vitest";
+import { it, expect } from "vitest";
 
 import { add } from './math';
 
 it('should summarize all number values in an array', () => {
-
+  const result = add([1,2,3]);
+  expect(result).toBe(6);
 });
