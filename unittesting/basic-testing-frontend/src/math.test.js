9c990a50ef85e7566108fc5b8abd861b806cd27c
Emily Baird <emilym.baird@gmail.com>
2022-06-28 22:23:26 +0100

    ex 24

--- unittesting/basic-testing-frontend/src/math.test.js
+++ unittesting/basic-testing-frontend/src/math.test.js
@@ -49,3 +49,14 @@ it('should throw an error if no value is passed into the function', () => {
 
   expect(resultFunction).toThrow();
 });
+
+it('should throw an error if provided with multiple arguments instead of an array', () => {
+  const num1 = 1;
+  const num2 = 2;
+
+  const resultFn = () => {
+    add(num1, num2);
+  };
+
+  expect(resultFn).toThrow();
+});
