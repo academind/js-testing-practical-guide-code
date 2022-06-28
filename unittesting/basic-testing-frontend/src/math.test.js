2423c5c9cbae6f97e9624cfe6f2d3fe00d75bec2
Emily Baird <emilym.baird@gmail.com>
2022-06-28 10:43:21 +0100

    ex 19

--- unittesting/basic-testing-frontend/src/math.test.js
+++ unittesting/basic-testing-frontend/src/math.test.js
@@ -13,3 +13,23 @@ it('should summarize all number values in an array', () => {
   // Assert
   expect(result).toBe(expectedResult);
 });
+
+it('should yield NaN if at least one invalid number is provided', () => {
+// Arrange
+  const inputs = ['invalid', 1];
+
+  // Act
+  const result = add(inputs);
+
+  // Assert
+  expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+  const numbers = ["1", "2"];
+  const expectedResult = numbers.reduce((prevValue, curValue) => prevValue + +curValue, 0);
+
+  const result = add(numbers);
+
+  expect(result).toBe(expectedResult);
+});
