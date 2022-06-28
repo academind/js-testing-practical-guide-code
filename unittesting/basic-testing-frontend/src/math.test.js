e29f9135f0cbcb84adae58f2d79299a0b9cb7f09
Emily Baird <emilym.baird@gmail.com>
2022-06-28 22:17:05 +0100

    ex 22-23

--- unittesting/basic-testing-frontend/src/math.test.js
+++ unittesting/basic-testing-frontend/src/math.test.js
@@ -33,3 +33,19 @@ it('should yield a correct sum if an array of numeric string values is provided'
 
   expect(result).toBe(expectedResult);
 });
+
+it('should yield 0 if an empty array is provided', () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).toBe(0);
+});
+
+it('should throw an error if no value is passed into the function', () => {
+  const resultFunction = () => {
+    add();
+  };
+
+  expect(resultFunction).toThrow();
+});
